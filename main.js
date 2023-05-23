41fbe5cbb2a1eaac11e80db77f73273303c92302
Bruno Rubiños <brunorubinosperez@gmail.com>
2023-05-22 22:13:03 -0500

    Fusion de mobileMenu con la nav y ajuste responsive

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const iconMenu = document.querySelector(".icon-menu");
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
 }
 
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
+}
+
 navEmail.addEventListener("click",toggleDesktopMenu);
+iconMenu.addEventListener("click",toggleMobileMenu);
 
 
