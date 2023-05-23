25ac31dd2d6480193be508d05ec7f53bdba9db09
Bruno Rubiños <brunorubinosperez@gmail.com>
2023-05-22 23:32:27 -0500

    Integracion de aside carrito de compras

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-const mobileMenu = document.querySelector(".mobile-menu");
 const iconMenu = document.querySelector(".icon-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const iconShoppingCar = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
 
 function toggleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive');
+  const isProductDetailOpen = !productDetail.classList.contains("inactive");
+
+  if (isProductDetailOpen) {
+    productDetail.classList.add("inactive");
+  }
+
+  desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-  mobileMenu.classList.toggle('inactive');
+  mobileMenu.classList.toggle("inactive");
+  productDetail.classList.add("inactive");
 }
 
-navEmail.addEventListener("click",toggleDesktopMenu);
-iconMenu.addEventListener("click",toggleMobileMenu);
+function toggleProductDetail() {
+  const isMobileMenuOpen = !mobileMenu.classList.contains("inactive");
+  const isDesktopMenuOpen = !desktopMenu.classList.contains("inactive");
 
+  if (isMobileMenuOpen) {
+    mobileMenu.classList.add("inactive");
+  }
 
+  if (isDesktopMenuOpen) {
+    desktopMenu.classList.add("inactive");
+  }
+
+  productDetail.classList.toggle("inactive");
+}
+
+navEmail.addEventListener("click", toggleDesktopMenu);
+iconMenu.addEventListener("click", toggleMobileMenu);
+iconShoppingCar.addEventListener("click", toggleProductDetail);
